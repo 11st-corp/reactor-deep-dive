cea0a53c3c3a6019cbe15ae327a799e737f653b2
G1 <g1.tommy.jeon@gmail.com>
2023-04-13 22:12:15 +0900

    test: c11_Batching

--- exercises/src/test/java/c11_Batching.java
+++ exercises/src/test/java/c11_Batching.java
@@ -28,10 +28,9 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void batch_writer() {
-        //todo do your changes here
-        Flux<Void> dataStream = null;
-        dataStream();
-        writeToDisk(null);
+        Flux<Void> dataStream = dataStream()
+                .buffer(10)
+                .flatMap(this::writeToDisk);
 
         //do not change the code below
         StepVerifier.create(dataStream)
@@ -49,10 +48,9 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void command_gateway() {
-        //todo: implement your changes here
-        Flux<Void> processCommands = null;
-        inputCommandStream();
-        sendCommand(null);
+        Flux<Void> processCommands = inputCommandStream()
+                .groupBy(Command::getAggregateId)
+                .flatMap(aggregateCommands -> aggregateCommands.concatMap(this::sendCommand));
 
         //do not change the code below
         Duration duration = StepVerifier.create(processCommands)
@@ -69,7 +67,9 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void sum_over_time() {
         Flux<Long> metrics = metrics()
-                //todo: implement your changes here
+                .window(Duration.ofSeconds(1))
+                .concatMap(window -> window.reduce(0L, Long::sum))
+                .doOnNext(System.out::println)
                 .take(10);
 
         StepVerifier.create(metrics)
